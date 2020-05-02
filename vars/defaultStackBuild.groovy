704fe59e3ada5d558313344c44ede8ffe98ef3bd
root <root@dc1>
2020-05-02 19:01:46 +0000

    changes

--- vars/defaultStackBuild.groovy
+++ vars/defaultStackBuild.groovy
@@ -5,67 +5,72 @@ properties([
     disableConcurrentBuilds()
 ])
 
+def tokens = "${masterBuild}".tokenize('/')
+def team = tokens[0]
+def repo = tokens[1]
+def BRANCH = tokens[2]
+def REPO_URL = "git@github.com:${team}/${repo}.git"
+def PATH = "${env.JOB_NAME}"-"${repo}/"
+
 node('dood') {
+    stage('variables') {
+        echo "Terraform Version = $TERRAFORM_VERSION"
+        echo "Ruby Version = $RUBY_VERSION"
+        echo "Container ID = $DOCKER_CONTAINER_ID"
+        echo "WORKSPACE = $WORKSPACE"
+	echo "Terraform PATH = ${PATH}"
+    }
 
-	    stage('variables') {
-	        echo "Terraform Version = $TERRAFORM_VERSION"
-		echo "Ruby Version = $RUBY_VERSION"
-		echo "Container ID = $DOCKER_CONTAINER_ID"
-		echo "WORKSPACE = $WORKSPACE"
-	    }
-
-            stage('Checkout Code') {
-                checkout scm
+    stage('Checkout Code') {
+        checkout scm
+    }
+    stage('Resolve Dependencies') {
+        sh('ls -l')
+        sh("cp ${PATH}/dependency.json .")
+        sh("cp ${PATH}/vars.tfvars .")
+        module_name = sh(script: 'jq --raw-output .dependencies[0].name dependency.json', returnStdout: true).trim()
+        moduleURL = sh(script: 'jq --raw-output .dependencies[0].url dependency.json', returnStdout: true).trim()
+        submodulePath = sh(script: 'jq --raw-output .dependencies[0].modulepath dependency.json', returnStdout: true).trim()
+        moduleVersion = sh(script: 'jq --raw-output .dependencies[0].version_requirement dependency.json', returnStdout: true).trim()
+        if (moduleURL != null) {
+            stage('Download Dependencies') {
+                sh("printenv | sort")
+                sh("git clone ${moduleURL}")
+                sh("cd ${module_name} && git checkout tags/v${moduleVersion}")
+                sh("cp -r ${module_name}/${submodulePath}/* .")
+                sh('ls -l')
             }
-
-            stage('Resolve Dependencies') {
-		module_name = sh(script: 'jq --raw-output .dependencies[0].name metadata.json', returnStdout: true).trim()
-                moduleURL = sh(script: 'jq --raw-output .dependencies[0].url metadata.json', returnStdout: true).trim()
-                submodulePath = sh(script: 'jq --raw-output .dependencies[0].modulepath metadata.json', returnStdout: true).trim()
-                moduleVersion = sh(script: 'jq --raw-output .dependencies[0].version_requirement metadata.json', returnStdout: true).trim()
-                if (moduleURL != null) {
-                    stage('Download Dependencies') {
-			sh("printenv | sort")
-			workdir = "${env.WORKSPACE}"
-                        sh("git clone ${moduleURL}")
-                        sh("cd ${module_name} && git checkout tags/v${moduleVersion} && cd -")
-                        sh "cp -r ${module_name}/${submodulePath}/* ${workdir}"
-                    }
+        }
+    }
+        stage('Are we building?') {
+	   sh 'git log -1 --pretty=%B > git_message'
+           if (!readFile('git_message').startsWith('[blacksmith]')) {
+		stage('Setup Gitconfig') {
+		    sh("git remote set-url origin ${REPO_URL}")
+		    sh("git config user.email blacksmith@jenkins.local")
+		    sh("git config user.name 'BlackSmith'")
+		}
+                stage('Validate Terraform') {
+                    sh 'terraform init'
+                    sh 'terraform validate'
                 }
-            }
-            stage('Are we building?') {
-                sh 'git log -1 --pretty=%B > git_message'
-                if (!readFile('git_message').startsWith('[blacksmith]')) {
-                    stage('Setup Gitconfig') {
-                        tokens = "${env.JOB_NAME}".tokenize('/')
-                        org = tokens[0]
-                        repo = tokens[1]
-                        branch = tokens[2]
-                        sh("git remote set-url origin git@github.com:sahilss429/myservice-app.git")
-                        sh("git config user.email sahilss429@gmail.com")
-                        sh("git config user.name 'sahilss429'")
-                    }
-                    stage('Validate Terraform') {
-                        sh 'terraform init'
-                        sh 'terraform validate'
-                    }
-                    stage('Terraform Plan') {
-		        echo "terraform plan -lock=true -var-file=vars.tf -input=false -out=plan.tf"
-                    }
-                    stage('Apply Changes') {
-                        input 'Apply Changes?'
-                    }
-                    stage('Terraform Apply') {
-                        echo "Not running apply this time command could be terraform apply -lock=true -var-file=vars.tf -input=false plan.tf"
-                    }
-                    stage('Update Tag') {
-                        sh 'rake tag'
-                    }
-                     stage('Publish Tag') {
-                        sh 'git checkout -b publish_tmp && git checkout -B master publish_tmp && rake publish && git branch -d publish_tmp && git push --tags origin master'
-                     }
+                stage('Terraform Plan') {
+		          echo "terraform plan -lock=true -var-file=vars.tf -input=false -out=plan.tf"
                 }
-            }
+                stage('Apply Changes') {
+                    input 'Apply Changes?'
+                }
+                stage('Terraform Apply') {
+                    echo "Not running apply this time command could be terraform apply -lock=true -var-file=vars.tf -input=false plan.tf"
+                }
+                stage('Update Tag') {
+                    sh 'rake tag'
+                }
+                stage('Publish Tag') {
+                    sh 'git checkout -b publish_tmp && git checkout -B master publish_tmp && rake publish && git branch -d publish_tmp && git push --tags origin master'
+                }
+	   }
+        }
 }
 
 }
