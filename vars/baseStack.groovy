b93e9919f8b8c58296a752eb71a11f2b986f90ca
root <root@dc1>
2020-05-02 16:31:16 +0000

    changes

--- vars/baseStack.groovy
+++ vars/baseStack.groovy
@@ -20,8 +20,6 @@ def repo = tokens[1]
 def BRANCH = tokens[2]
 def REPO_URL = "git@github.com:/${team}/${repo}.git"
 
-library identifier: 'pipeline-shared-library@master', retriever: modernSCM([$class: 'GitSCMSource', credentialsId: '', remote: 'git@github.com:sahilss429/pipeline-shared-library.git', traits: [gitBranchDiscovery()]])
-
 properties([
     [$class: 'GithubProjectProperty', displayName: '', projectUrlStr: "${REPO_URL}"],
     buildDiscarder(logRotator(numToKeepStr: '5')),
