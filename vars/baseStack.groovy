dc816bd2fa72b5f564789c9f14e4b56c257781ce
root <root@dc1>
2020-05-03 09:22:59 +0000

    change

--- vars/baseStack.groovy
+++ vars/baseStack.groovy
@@ -37,7 +37,8 @@ node('dood') {
     }
     stage('Are we building?') {
         def dir_paths = checkFolderForDiffs()
-	echo "$dir_paths"
+	def job_paths = "$dir_paths".split(' ').join(',')
+	echo "$job_paths"
         sh 'git log -1 --pretty=%B > git_message'
         if (!readFile('git_message').startsWith('[blacksmith]') && dir_paths != "") {
             stage('Setup Gitconfig') {
