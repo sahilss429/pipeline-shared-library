c2517e603f1702b89e0b71de036f050a715516e1
sahilss429 <sahilss429@gmail.com>
2022-03-11 23:51:47 +0530

    Update applicationDeploy.groovy

--- vars/applicationDeploy.groovy
+++ vars/applicationDeploy.groovy
@@ -38,7 +38,7 @@ properties([
       stage('Production Equivalent Environment') {
         ansiColor('xterm') {
           try {
-            withCredentials([string(credentialsId: 'jenkins-deploy', variable: 'ARGOCD_AUTH_TOKEN'),usernamePassword(credentialsId: "argosecret", passwordVariable: 'argopassword', usernameVariable: 'argouser'),sshUserPrivateKey(credentialsId: "test", keyFileVariable: 'private_key')]) {
+            withCredentials([usernamePassword(credentialsId: "argosecret", passwordVariable: 'argopassword', usernameVariable: 'argouser'),sshUserPrivateKey(credentialsId: "test", keyFileVariable: 'private_key')]) {
               sh("make INFRA_REPO=${infra_repo} SERVICE_NAME=${SERVICE_NAME} install")
               sh("make INFRA_REPO=${infra_repo} SERVICE_NAME=${SERVICE_NAME} production")
             }
@@ -54,7 +54,7 @@ properties([
       stage('Non Production Environment') {
         ansiColor('xterm') {
           try {
-            withCredentials([usernamePassword(string(credentialsId: 'jenkins-deploy', variable: 'ARGOCD_AUTH_TOKEN'),usernamePassword(credentialsId: "argosecret", passwordVariable: 'argopassword', usernameVariable: 'argouser'),sshUserPrivateKey(credentialsId: "test", keyFileVariable: 'private_key')]) {
+            withCredentials([usernamePassword(credentialsId: "argosecret", passwordVariable: 'argopassword', usernameVariable: 'argouser'),sshUserPrivateKey(credentialsId: "test", keyFileVariable: 'private_key')]) {
               sh("make INFRA_REPO=${infra_repo} SERVICE_NAME=${SERVICE_NAME} install")
               sh("make INFRA_REPO=${infra_repo} SERVICE_NAME=${SERVICE_NAME} nonprod")
             }
